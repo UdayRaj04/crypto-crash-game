98d98257c284fc5da148caabd9c9fe6d5a389ad4
udayraj04 <sharmarajuday@gmail.com>
2025-07-28 23:45:16 +0530

    up

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,10 +1,6 @@
 import { io } from "socket.io-client";
 
 const socket = io(
-  window.location.hostname ===
-   "localhost"
-    ? "http://localhost:5000"
-    : 
     "https://crypto-crash-game-nvw3.onrender.com"
 );
 
